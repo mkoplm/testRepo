6f9333d55901e27a2be33e8d725dfaf3763b6f99
mkoplm <mkoplm256@gmail.com>
2014-04-01 22:32:55 +0900

    test용 다올림

---
+++ testImport/src/main/java/ucamp19a/testImport/App.java
@@ -0,0 +1,18 @@
+package ucamp19a.testImport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!1" );
+        System.out.println( "Hello World!2" );
+        System.out.println( "Hello World!3" );
+        System.out.println( "Hello World!4" );
+        System.out.println( "Hello World!5" );
+    }
+}
